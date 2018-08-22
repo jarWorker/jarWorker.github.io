594f0b509e88632b713068fe3df80ef57734c10e
389390246hkz@sina.com <389390246hkz@sina.com>
2018-08-22 11:16:35 +0800

    Site updated: 2018-08-22 11:16:35

--- photos/ins.js
+++ photos/ins.js
@@ -116,8 +116,8 @@
         var data = res.list[j].arr;
         var liTmpl = "";
         for (var i = 0, len = data.link.length; i < len; i++) {
-          var minSrc = 'https://raw.githubusercontent.com/jarWorker/hexo_photos_jar/master/min_photos/' + data.link[i];
-          var src = 'https://raw.githubusercontent.com/jarWorker/hexo_photos_jar/master/photos/' + data.link[i];
+          var minSrc = 'https://github.com/jarWorker/hexo_photos_jar/blob/master/photos/min_photos/' + data.link[i];
+          var src = 'https://github.com/jarWorker/hexo_photos_jar/blob/master/photos/photos/' + data.link[i];
           var type = data.type[i];
           var target = src + (type === 'video' ? '.mp4' : '.jpg');
           src += '';
